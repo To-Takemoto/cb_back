14ca16014254b489fe4f94a13e82e00ff41e093a
take <s2322039@stu.musashino-u.ac.jp>
2025-05-12 00:10:04 +0900

    mold: 表示の際にいちいちDBにアクセスしなくて良いようにmessageのキャッシュを実装

--- src/usecase/chat_interaction/main.py
+++ src/usecase/chat_interaction/main.py
@@ -13,6 +13,7 @@ class ChatInteraction:
         self.chat_repo = chat_repo
         self.llm_client = llm_client
         self.structure_handler = StructureHandle(self.chat_repo)
+        self.message_store:list[MessageEntity] = []
 
     def start_new_chat(self, initial_strings: str = None) -> None:
         initial_message = MessageEntity(None, None, Role.SYSTEM, initial_strings)
@@ -23,6 +24,7 @@ class ChatInteraction:
     async def continue_chat(self, user_message_strings: str) -> MessageEntity:
         user_message = MessageEntity(None, None, Role.USER, user_message_strings)
         filled_user_message = self.chat_repo.save_message(self.structure_handler.chat_tree.uuid, user_message)
+        self._cache_messsage(filled_user_message)
         self.structure_handler.append_message(filled_user_message)
         self.chat_repo.update_tree(self.structure_handler.chat_tree)
         fllatten_chat_history_uuid = self.structure_handler.get_current_path()
@@ -30,6 +32,7 @@ class ChatInteraction:
         async with self.llm_client:
             llm_message, full_data = await self.llm_client.complete_message(flatten_chat_history)
         filled_llm_message = self.chat_repo.save_message(self.structure_handler.chat_tree.uuid, llm_message, full_data)
+        self._cache_messsage(filled_llm_message)
         self.structure_handler.append_message(filled_llm_message)
         self.chat_repo.update_tree(self.structure_handler.chat_tree)
         return filled_llm_message
@@ -41,3 +44,13 @@ class ChatInteraction:
 
     def select_message(self, message_uuid):
         self.structure_handler.select_node(message_uuid)
+
+    def find_message_from_cache(self, message_uuid: str) -> MessageEntity:
+        for message in self.message_store:
+            if str(message.uuid) == message_uuid:
+                return message
+        else:
+            raise ValueError("uuidを持つmessageなし。")
+
+    def _cache_messsage(self, message: MessageEntity) -> None:
+        self.message_store.append(message)
